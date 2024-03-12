c8d477328ddcad7f7f8ad72d5581d284a476dd66
AnikaPuri <42912590+AnikaPuri@users.noreply.github.com>
2024-03-12 16:31:08 -0400

    push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <title>Anika Puri: Personal Site and Portfolio</title>
 <h1>Anika Puri</h1>
 <p>Hi! My name is Anika Puri and I'm currently a sophomore at MIT majoring in 6-4.</p>
-<img src="/Users/anikapuri/6.C35 Visualization & Society/GitHub/my-portfolio/static/images/AnikaPuri_Photo (2).jpg" alt="picture of Anika Puri in the woods jpg" width="300" height=auto />
+<img src="images/AnikaPuri_Photo (2).jpg" alt="picture of Anika Puri in the woods jpg" width="300" height=auto />
 
 <h2>Latest Projects</h2>
     <div class="projects">
