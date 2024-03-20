87ea661cfaec773991d23c6771bbb578eefd735b
AnikaPuri <42912590+AnikaPuri@users.noreply.github.com>
2024-03-20 16:15:00 -0400

    push

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -1,1 +1,128 @@
-Hello from Pie.svelte
+<script>
+
+import * as d3 from 'd3';
+
+export let data = [];
+
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+	startAngle: 0,
+	endAngle: 2 * Math.PI
+});
+
+let sliceGenerator = d3.pie().value(d => d.value);
+
+// Make sure the variable definition is *outside* the block
+let arcData; 
+let arcs;
+
+$: {
+	// Initialize to an empty object every time this runs
+	arcData = sliceGenerator(data);
+    arcs = arcData.map(d => arcGenerator(d));
+
+	// Calculate rolledData and pieData based on filteredProjects here
+}
+
+
+let colors = d3.scaleOrdinal(d3.schemePastel1);
+
+export let selectedIndex = -1;
+
+
+</script>
+
+
+<div class = "container">
+    <svg viewBox="-50 -50 100 100">
+        {#each arcs as arc, index}
+        
+	<path d={arc} fill={ colors(index) }
+        class:selected={selectedIndex === index}
+	      on:click={e => selectedIndex = selectedIndex === index ? -1 : index
+	      } />
+{/each}
+    </svg> 
+ 
+    <ul class="legend">
+      {#each data as d, index}
+        <li style="--color: { colors(index) }">
+          <span class="swatch"></span>
+          {d.label} <em>({d.value})</em>
+        </li>
+      {/each}
+    </ul>
+</div>
+
+
+
+
+<style>
+
+.swatch {
+    /* aspect-ratio: 1 /1; */
+    width:1em;
+    height:1em;
+    background-color: var(--color);
+    border-radius: 50%;
+    align-items: center;
+    border-spacing: gap;
+    display: flex;
+
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(9em, 1fr));
+    /* max-width: 100%;  */
+    outline: 0.1em solid black;
+    outline-offset: 1em;
+  }
+
+  li {
+    display: flex;
+    gap:0.5em;;
+  }
+
+  svg {
+    max-width: 20em;
+    width:40vw;
+    margin-block: 2em;
+
+
+    /* Do not clip shapes outside the viewBox */
+    overflow: visible;
+  }
+
+  .container {
+    display: flex;
+    align-items: center;
+    gap: 4em;
+    padding: 0em;
+  }
+
+  .legend {
+    flex: 1
+    /* list-style-type: none; */
+  }
+
+  svg:has(path:hover) {
+	path:not(:hover) {
+		opacity: 50%;
+	}
+}
+path {
+	transition: 300ms;
+}
+
+.selected {
+	--color: oklch(60% 45% 0) !important;
+
+	&:is(path) {
+		fill: var(--color);
+	}
+}
+
+  </style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,20 +2,83 @@
 
 import Project from "$lib/Project.svelte";
 
+import * as d3 from 'd3';
+
 import Pie from "$lib/Pie.svelte";
+
+
+// let data = [
+// 	{ value: 1, label: "apples" },
+// 	{ value: 2, label: "oranges" },
+// 	{ value: 3, label: "mangos" },
+// 	{ value: 4, label: "pears" },
+// 	{ value: 5, label: "limes" },
+// 	{ value: 5, label: "cherries" }
+// ];
+
+let filteredProjects;
+$: filteredProjects = projects.filter(project => {
+	let values = Object.values(project).join("\n").toLowerCase();
+	return values.includes(query.toLowerCase());
+});
+
+let rolledData;
+
+// Make sure the variable definition is *outside* the block
+let pieData;
+
+$: {
+	// Initialize to an empty object every time this runs
+	pieData = {};
+  rolledData = d3.rollups(filteredProjects, v => v.length, d => d.year);
+
+  pieData = rolledData.map(([year, count]) => {
+	return { value: count, label: year };
+});
+
+	// Calculate rolledData and pieData based on filteredProjects here
+}
+let query = "";
+
+
+
+let selectedYearIndex = -1;
+
+let selectedYear;
+$: selectedYear = selectedYearIndex > -1 ? pieData[selectedYearIndex].label : null;
+
+
+let filteredProjectsYear;
+$: filteredProjectsYear = filteredProjects.filter(project => {
+  if (selectedYearIndex !== -1)
+   return project.year === selectedYear;
+  else
+    return true;
+})
+
+
 
 </script>
 
 <title>Project</title>
     <h1>{projects.length} Projects</h1>
 
-    <Pie
-    <svg viewBox="-50 -50 100 100">
-      <circle cx="0" cy="0" r="50" fill="red" /> </svg> />
+  <Pie data={pieData} bind:selectedIndex={selectedYearIndex} />
+
+
+  <input type="search" bind:value={query}
+  aria-label="Search projects" placeholder="🔍 Search projects…" />
+
+
+
     <div class="projects">
-      {#each projects as p}
+      {#each filteredProjectsYear as p}
         <article>
           <Project data={p} />
         </article>
       {/each}
     </div>
+
+  <style>
+
+  </style>
